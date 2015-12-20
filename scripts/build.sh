162a8ff42b83027aa5343641bc60156689807b84
Hiroaki Nakamura <hnakamur@gmail.com>
2015-12-21 01:49:05 +0900

    Use rpmbuild -bs to build the srpm file

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,16 @@ project_name=varnish-head
 rpm_name=varnish
 arch=x86_64
 
+copr_project_description="HEAD version of Varnish High-performance HTTP accelerator"
+
+copr_project_instructions="\`\`\`
+sudo curl -sL -o /etc/yum.repos.d/${COPR_USERNAME}-${copr_project_name}.repo https://copr.fedoraproject.org/coprs/${COPR_USERNAME}/${copr_project_name}/repo/epel-7/${COPR_USERNAME}-${copr_project_name}-epel-7.repo
+\`\`\`
+
+\`\`\`
+sudo yum install ${rpm_name}
+\`\`\`"
+
 spec_file=${rpm_name}.spec
 mock_chroot=epel-7-${arch}
 
@@ -24,27 +34,25 @@ topdir=`rpm --eval '%{_topdir}'`
 topdir_in_chroot=/builddir/build
 
 download_source_files() {
-  # NOTE: Edit commands here.
-  VARNISH_HEAD_GIT_COMMIT=`awk '/^%define\s+varnish_git_commit/ {print $3}' ${topdir}/SPECS/${spec_file}`
-  cd ${topdir}/SOURCES
-  curl -sLO https://github.com/varnish/Varnish-Cache/archive/${VARNISH_HEAD_GIT_COMMIT}.tar.gz#/Varnish-Cache-${VARNISH_HEAD_GIT_COMMIT}.tar.gz
+  source_urls=`rpmspec -P ${topdir}/SPECS/${spec_file} | awk '/^Source[0-9]*:\s*http/ {print $2}'`
+  for source_url in $source_urls; do
+    source_file=${source_url##*/}
+    (cd ${topdir}/SOURCES && if [ ! -f ${source_file} ]; then curl -sLO ${source_url}; fi)
+  done
 }
 
 build_srpm() {
-  version=`rpmspec -P ${topdir}/SPECS/${spec_file} | awk '$1=="Version:" { print $2 }'`
   download_source_files
-  /usr/bin/mock -r ${mock_chroot} --init
-  /usr/bin/mock -r ${mock_chroot} --no-clean --buildsrpm --spec "${topdir}/SPECS/${spec_file}" --sources "${topdir}/SOURCES/"
-  release=`/usr/bin/mock -r ${mock_chroot} --chroot "rpmspec -P ${topdir_in_chroot}/SPECS/${spec_file}" | awk '$1=="Release:" { print $2 }'`
+  rpmbuild -bs "${topdir}/SPECS/${spec_file}"
+  version=`rpmspec -P ${topdir}/SPECS/${spec_file} | awk '$1=="Version:" { print $2 }'`
+  release=`rpmspec -P ${topdir}/SPECS/${spec_file} | awk '$1=="Release:" { print $2 }'`
   rpm_version_release=${version}-${release}
   srpm_file=${rpm_name}-${rpm_version_release}.src.rpm
-  mock_result_dir=/var/lib/mock/${mock_chroot}/result
-  cp ${mock_result_dir}/${srpm_file} ${topdir}/SRPMS/
 }
 
 build_rpm_with_mock() {
   build_srpm
-  /usr/bin/mock -r ${mock_chroot} --no-clean --rebuild ${topdir}/SRPMS/${srpm_file}
+  /usr/bin/mock -r ${mock_chroot} --rebuild ${topdir}/SRPMS/${srpm_file}
 
   mock_result_dir=/var/lib/mock/${mock_chroot}/result
   if [ -n "`find ${mock_result_dir} -maxdepth 1 -name \"${rpm_name}-*${rpm_version_release}.${arch}.rpm\" -print -quit`" ]; then
@@ -72,14 +80,8 @@ build_rpm_on_copr() {
     # NOTE: Edit description here. You may or may not need to edit instructions.
     curl -s -X POST -u "${COPR_LOGIN}:${COPR_TOKEN}" \
       --data-urlencode "name=${project_name}" --data-urlencode "${mock_chroot}=y" \
-      --data-urlencode "description=HEAD version of Varnish High-performance HTTP accelerator" \
-      --data-urlencode "instructions=\`\`\`
-sudo curl -sL -o /etc/yum.repos.d/${COPR_USERNAME}-${project_name}.repo https://copr.fedoraproject.org/coprs/${COPR_USERNAME}/${project_name}/repo/epel-7/${COPR_USERNAME}-${project_name}-epel-7.repo
-\`\`\`
-
-\`\`\`
-sudo yum install ${rpm_name}
-\`\`\`" \
+      --data-urlencode "description=${copr_project_description}" \
+      --data-urlencode "instructions=${copr_project_instructions}" \
       https://copr.fedoraproject.org/api/coprs/${COPR_USERNAME}/new/
   fi
   # Add a new build on copr with uploading a srpm file.
